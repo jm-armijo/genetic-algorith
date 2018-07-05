bc3ac32723231fad24e5027e85b577863a70d570
Jose Miguel Armijo <jm.armijo.f@gmail.com>
2018-07-06 08:05:23 +1000

    Adding comments to class Individual

--- Individual.cpp
+++ Individual.cpp
@@ -46,6 +46,8 @@ void Individual::mutate()
 {
     unsigned gen_number = Random::UnsignedUniform(0, m_num_genes-1);
     m_genes[gen_number].mutate();
+
+    // Check that the individual's size remain unchanged after mutation
     assert (m_genes.size() == m_num_genes);
 }
 
@@ -63,11 +65,29 @@ void Individual::fitness(const std::vector<double> &args, double expected)
 {
     double fitness = Gene::fitnessDNA(m_genes, args, expected);
 
+    // Calculates average n values without the need of having all n values up front.
+    // This prevents overflow for high values, but may lose precision.
+    // Exampe for n = 3 (m_num_values = 2 when entering the function)
+    //
+    // x =  a + b + c   a + b   c   a + b   2   c    a + b
+    //     ---------- = ----- + - = ----- * - + - =  -----
+    //          3         3     3     2     3   3      2          c
+    //                                              ------- * 2 + -
+    //                                                 3          3
+
+    // Increase number of values to get average
     ++m_num_evals;
+
+    // Divide the old average by the new value of n
     m_value /= m_num_evals;
+
+    // Then multiply the result by the old value of n
     m_value *= (m_num_evals - 1);
+
+    // Finally add the new fitness divided by the new value of n
     double new_value = fitness / m_num_evals;
     m_value += new_value;
+
     return;
 }
 
